cf971a7bff3a45880d82b09465c221049a165784
xiamidaxia <xiamidaxia@icloud.com>
2025-05-13 12:47:09 +0800

    chore: support react 16

---
+++ apps/demo-react-16/.eslintrc.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@flowgram.ai/eslint-config');
+
+module.exports = defineConfig({
+  preset: 'web',
+  packageRoot: __dirname,
+  rules: {
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    'react/no-deprecated': 'off',
+  },
+  settings: {},
+});
---
+++ apps/demo-react-16/src/components/node-add-panel.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { WorkflowDragService, useService } from '@flowgram.ai/free-layout-editor';
+
+const cardkeys = ['Node1', 'Node2'];
+
+export const NodeAddPanel: React.FC = (props) => {
+  const startDragSerivce = useService<WorkflowDragService>(WorkflowDragService);
+
+  return (
+    <div className="demo-free-sidebar">
+      {cardkeys.map((nodeType) => (
+        <div
+          key={nodeType}
+          className="demo-free-card"
+          onMouseDown={(e) =>
+            startDragSerivce.startDragCard(nodeType, e, {
+              data: {
+                title: `New ${nodeType}`,
+                content: 'xxxx',
+              },
+            })
+          }
+        >
+          {nodeType}
+        </div>
+      ))}
+    </div>
+  );
+};
