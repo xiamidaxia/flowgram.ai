be60c53f08427d6e81df5cc5764901a848678570
xiamidaxia <xiamidaxia@icloud.com>
2025-06-20 17:44:25 +0800

    feat(free-group-plugin): add initGroupJSON config

--- packages/plugins/free-group-plugin/src/type.ts
+++ packages/plugins/free-group-plugin/src/type.ts
@@ -1,7 +1,12 @@
 import { FC } from 'react';
 
+import { WorkflowNodeEntity, WorkflowNodeJSON } from '@flowgram.ai/free-layout-core';
+
 export interface WorkflowGroupPluginOptions {
   groupNodeRender: FC;
   disableGroupShortcuts?: boolean;
   disableGroupNodeRegister?: boolean;
+  initGroupJSON?: (json: WorkflowNodeJSON, nodes: WorkflowNodeEntity[]) => WorkflowNodeJSON;
 }
+
+export const WorkflowGroupPluginOptions = Symbol('WorkflowGroupPluginOptions');
