0027c2e45242d38ef28158d2fcb35d65c96b09f9
Louis Young <63398145+louisyoungx@users.noreply.github.com>
2025-06-23 12:42:39 +0800

    fix(core): some device touches is not array (#398)

--- packages/canvas-engine/core/src/core/utils/mouse-touch-event.ts
+++ packages/canvas-engine/core/src/core/utils/mouse-touch-event.ts
@@ -1,6 +1,6 @@
 export namespace MouseTouchEvent {
   export const isTouchEvent = (event: TouchEvent | React.TouchEvent): event is TouchEvent =>
-    Array.isArray(event?.touches) && event.touches.length > 0;
+    'touches' in event;
 
   export const touchToMouseEvent = (event: Event): MouseEvent | Event => {
     if (!isTouchEvent(event as TouchEvent)) {
