ec892a3b39ebb3f572d21eed4883ad74b0de9403
xiamidaxia <xiamidaxia@icloud.com>
2025-07-14 19:48:03 +0800

    feat(demo): add slot adder

---
+++ apps/demo-fixed-layout-simple/src/components/slot-adder.tsx
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+ * SPDX-License-Identifier: MIT
+ */
+
+import { nanoid } from 'nanoid';
+import {
+  type FlowNodeEntity,
+  FlowNodeRenderData,
+  FlowDocument,
+  useService,
+} from '@flowgram.ai/fixed-layout-editor';
+import { Button } from '@douyinfe/semi-ui';
+import { IconPlus } from '@douyinfe/semi-icons';
+
+interface PropsType {
+  node: FlowNodeEntity;
+}
+
+export function SlotAdder(props: PropsType) {
+  const { node } = props;
+
+  const nodeData = node.firstChild?.getData<FlowNodeRenderData>(FlowNodeRenderData);
+  const document = useService(FlowDocument) as FlowDocument;
+
+  async function addPort() {
+    document.addNode({
+      id: nanoid(5),
+      type: 'custom',
+      parent: node,
+      data: {
+        title: 'Custom',
+        content: 'custom content',
+      },
+    });
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        background: 'var(--semi-color-bg-0)',
+      }}
+      onMouseEnter={() => nodeData?.toggleMouseEnter()}
+      onMouseLeave={() => nodeData?.toggleMouseLeave()}
+    >
+      <Button
+        onClick={() => {
+          addPort();
+        }}
+        size="small"
+        icon={<IconPlus />}
+      />
+    </div>
+  );
+}
