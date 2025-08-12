9ba1fc967097f1f3ed1a0863ca6b1875d906a041
xiamidaxia <xiamidaxia@icloud.com>
2025-08-12 18:02:50 +0800

     This is a combination of 2 commits.  This is the 1st commit message:
    feat(free-history-plugin): add change-line-data to history
    
     The commit message #2 will be skipped:
    
     feat(free-history-plugin): add change-line-data to history

--- packages/client/free-layout-editor/__tests__/history.test.ts
+++ packages/client/free-layout-editor/__tests__/history.test.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+ * SPDX-License-Identifier: MIT
+ */
+
 import { describe, it, expect } from 'vitest';
 import { WorkflowDocument } from '@flowgram.ai/free-layout-core';
 import { HistoryService } from '@flowgram.ai/free-history-plugin';
