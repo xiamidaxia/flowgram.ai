4e2a7a1b23179aaf2609d98b6c98fce83b910a4c
chenjiawei.inizio <289056872@qq.com>
2025-08-29 14:38:18 +0800

    feat: open ssg (#749)
    * chore: 构建产物push到gh-pages分支
    
    * chore: 打开ssg
    
    * chore: 减少构建依赖包
    
    * chore: save
    
    * chore: content write
    
    * chore: opt install
    
    * chore: manual add vercel config

--- apps/docs/rspress.config.ts
+++ apps/docs/rspress.config.ts
@@ -46,7 +46,40 @@ export default defineConfig({
       },
     },
   },
-  ssg: false,
+  ssg: {
+    experimentalExcludeRoutePaths: [
+      /\/auto-docs\//,
+      // these pages do not support SSR
+      // document is not defined
+      '/en/examples/node-form/basic',
+      '/en/examples/node-form/array',
+      '/en/examples/node-form/dynamic',
+      '/en/guide/getting-started/create-fixed-layout-simple',
+      '/en/guide/getting-started/create-free-layout-simple',
+      '/en/examples/node-form/effect',
+      '/en/guide/advanced/fixed-layout/composite-nodes',
+      '/en/examples/playground',
+      '/en/examples/fixed-layout/fixed-composite-nodes',
+      '/en/examples/fixed-layout/fixed-layout-simple',
+      '/en/examples/free-layout/free-layout-simple',
+      '/en/examples/fixed-layout/fixed-feature-overview',
+      '/en/examples/free-layout/free-feature-overview',
+
+      '/examples/node-form/basic',
+      '/examples/node-form/array',
+      '/examples/node-form/dynamic',
+      '/guide/getting-started/create-fixed-layout-simple',
+      '/guide/getting-started/create-free-layout-simple',
+      '/examples/node-form/effect',
+      '/guide/advanced/fixed-layout/composite-nodes',
+      '/examples/playground',
+      '/examples/fixed-layout/fixed-composite-nodes',
+      '/examples/fixed-layout/fixed-layout-simple',
+      '/examples/free-layout/free-layout-simple',
+      '/examples/fixed-layout/fixed-feature-overview',
+      '/examples/free-layout/free-feature-overview',
+    ],
+  },
   // locales 为一个对象数组
   locales: [
     {
