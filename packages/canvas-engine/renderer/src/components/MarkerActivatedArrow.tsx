58d0b7b1688e5bd2d04ea5d92f461df19d006270
xiamidaxia <xiamidaxia@icloud.com>
2025-09-18 16:35:36 +0800

    refactor(fixed-layout): optimize loop arrow style

--- packages/canvas-engine/renderer/src/components/MarkerActivatedArrow.tsx
+++ packages/canvas-engine/renderer/src/components/MarkerActivatedArrow.tsx
@@ -16,15 +16,19 @@ function MarkerActivatedArrow(props: { id?: string }): JSX.Element {
     <marker
       data-line-id={props.id}
       id={props.id || MARK_ACTIVATED_ARROW_ID}
-      markerWidth="11"
-      markerHeight="14"
-      refX="10"
-      refY="7"
+      markerWidth="6"
+      markerHeight="10"
+      width="6"
+      height="10"
       orient="auto"
+      refX="5"
+      refY="5"
     >
       <path
-        d="M9.6 5.2C10.8 6.1 10.8 7.9 9.6 8.8L3.6 13.3C2.11672 14.4125 0 13.3541 0 11.5L0 2.5C0 0.645898 2.11672 -0.412461 3.6 0.7L9.6 5.2Z"
-        fill={baseActivatedColor}
+        d="M1 1L4.58132 4.72268C4.7303 4.87755 4.7303 5.12245 4.58132 5.27732L1 9"
+        stroke={baseActivatedColor}
+        fill="none"
+        strokeLinecap="round"
       />
     </marker>
   );
